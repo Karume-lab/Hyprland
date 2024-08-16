28977a24d72a934077da3d0b92db9f8cc12edf6d
Karume-lab <danielkarume@outlook.com>
2024-08-16 10:37:28 +0300

    ci: :adhesive_bandage: rename script files

--- return.sh
+++ return.sh
@@ -3,5 +3,5 @@ sudo cp -r ./scripts /usr/local/bin
 sudo cp ./hypr/monitors.conf ~/.config/hypr/monitors.conf
 sudo cp ./hypr/input.conf ~/.config/hypr/input.conf
 
-echo "source = ~/.config/hypr/input.conf" >> ~/.config/hypr/hyprland.conf
+echo "source = ~/.config/hypr/userprefs.conf" >> ~/.config/hypr/userprefs.conf
 paru -Sy --needed - < ./apps.txt
