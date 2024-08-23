ea764c572b7ad599345e4b16f5b90b341a2523b6
Karume-lab <danielkarume@outlook.com>
2024-08-23 16:37:06 +0300

    fixes

--- return.sh
+++ return.sh
@@ -1,8 +1,3 @@
-sudo tail -n 2 fstab >> /etc/fstab
 sudo cp -r ./scripts /usr/local/bin
-sudo cp ./hypr/monitors.conf ~/.config/hypr/monitors.conf
 sudo cp ./hypr/userprefs.conf ~/.config/hypr/userprefs.conf
 paru -Sy --needed - < ./apps.txt
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install franciscolourenco/done
